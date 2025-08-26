1325109ad34288125b402286dcc472392c0c30fe
Theepob <121664383+Bee0Theepob@users.noreply.github.com>
2025-08-26 20:23:31 +0700

    fix img

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="flex flex-row mx-auto pb-[15px] justify-evenly">
         <Card venueName="The Bloom Pavilion" imgSrc="/img/bloom.jpg" />
         <Card venueName="Spark Space" imgSrc="/img/sparkspace.jpg" />
-        <Card venueName=" The Grand Table" imgSrc="/img/grandtable.jpg " />
+        <Card venueName=" The Grand Table" imgSrc="/img/grandtable.jpg" />
       </div>
     </main>
   );
